4b6856ef4abcbb93562f8cd5463ac8b84a667035
rheuser2112 <rheuser@kcc.com>
2015-03-04 19:37:23 +0100

    Version 3

--- plot4.R
+++ plot4.R
@@ -37,7 +37,7 @@ plot(mydf.1$DateTime, mydf.1$Sub_metering_1, type = "l",
 points(mydf.1$DateTime, mydf.1$Sub_metering_2, type = "l", col = "red")
 points(mydf.1$DateTime, mydf.1$Sub_metering_3, type = "l", col = "blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       cex = 0.75, box.lwd = 1,lty=c(1,1,1), lwd= c(1,1,1),col = c("black", "red", "blue") )
+       cex = 0.75, bty='n', lty=c(1,1,1), lwd= c(1,1,1),col = c("black", "red", "blue") )
 
 ## 4
 plot(mydf.1$DateTime, mydf.1$Global_reactive_power, type = "l", 
